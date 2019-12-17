75fe50ae2275b5cd6c56d1222d08e044951736e6
patrickkaelin <55581874+patrickkaelin@users.noreply.github.com>
2019-12-17 14:41:20 +0100

    Modified: new csv-file, some more python attempts

--- El Professor/app/views.py
+++ El Professor/app/views.py
@@ -1,17 +1,29 @@
 from app import app
 from flask import render_template
 
+
 @app.route("/")
 def index():
     return render_template("public/index.html")
+
 
 @app.route("/overview")
 def overview():
     return render_template("public/overview.html")
 
+
 @app.route("/modules")
 def modules():
-    return render_template("public/modules.html")
+
+    # Dictionary mit den Modulinformation
+    modulinformation = {
+        "MIKR": "schriftlich",
+        "ENG": "elektronisch",
+        "INMA": "projektarbeit (100%)"
+    }
+
+    return render_template("public/modules.html", modulinformation=modulinformation)
+
 
 @app.route("/newentry")
 def newentry():
